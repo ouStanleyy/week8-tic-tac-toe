58bda3d25478e185ee05d02a547c4afb6f8df6ff
stanley <ou.stanley@yahoo.com>
2022-04-14 20:45:49 -0400

    ttt completed.

--- starter/class/ttt.js
+++ starter/class/ttt.js
@@ -2,14 +2,14 @@ const Screen = require("./screen");
 const Cursor = require("./cursor");
 
 class TTT {
-
   constructor() {
-
     this.playerTurn = "O";
 
-    this.grid = [[' ',' ',' '],
-                 [' ',' ',' '],
-                 [' ',' ',' ']]
+    this.grid = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
 
     this.cursor = new Cursor(3, 3);
 
@@ -18,7 +18,7 @@ class TTT {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', TTT.testCommand);
+    Screen.addCommand("t", "test command (remove)", TTT.testCommand);
 
     Screen.render();
   }
@@ -29,18 +29,58 @@ class TTT {
   }
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
-    // Return false if the game has not ended
+    // Return false if no winner
 
+    const rotatedGrid = grid[0].map((val, index) =>
+      grid.map((row) => row[index]).reverse()
+    );
+
+    if (
+      grid[0][0] === grid[1][1] &&
+      grid[1][1] === grid[2][2] &&
+      grid[0][0] !== " "
+    )
+      return grid[0][0];
+
+    if (
+      grid[0][2] === grid[1][1] &&
+      grid[1][1] === grid[2][0] &&
+      grid[0][2] !== " "
+    )
+      return grid[0][2];
+
+    for (let row of rotatedGrid) {
+      let rowJoin = row.join("");
+
+      if (rowJoin === "XXX") return "X";
+      if (rowJoin === "OOO") return "O";
+    }
+
+    let tieCheck = [];
+
+    for (let row of grid) {
+      let rowJoin = row.join("");
+
+      if (rowJoin === "XXX") return "X";
+      if (rowJoin === "OOO") return "O";
+
+      for (let space of row) {
+        if ("XO".includes(space)) tieCheck.push(space);
+      }
+    }
+
+    if (tieCheck.length === 9) return "T";
+
+    return false;
   }
 
   static endGame(winner) {
-    if (winner === 'O' || winner === 'X') {
+    if (winner === "O" || winner === "X") {
       Screen.setMessage(`Player ${winner} wins!`);
-    } else if (winner === 'T') {
+    } else if (winner === "T") {
       Screen.setMessage(`Tie game!`);
     } else {
       Screen.setMessage(`Game Over`);
@@ -48,7 +88,6 @@ class TTT {
     Screen.render();
     Screen.quit();
   }
-
 }
 
 module.exports = TTT;
