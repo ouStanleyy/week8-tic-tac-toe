e433a94aebffe7e3bdc0e5e3c1952964a9812d51
stanley <ou.stanley@yahoo.com>
2022-04-14 21:00:41 -0400

    Started working on computer-player.

--- starter/class/computer-player.js
+++ starter/class/computer-player.js
@@ -1,27 +1,25 @@
-
 class ComputerPlayer {
-
   static getValidMoves(grid) {
-    // Your code here
+    const validMoves = [];
+
+    for (let row of grid) {
+      let rowJoin = row.join("");
+      validMoves.push(...rowJoin);
+    }
+    return validMoves;
   }
 
   static randomMove(grid) {
-
     // Your code here
   }
 
   static getWinningMoves(grid, symbol) {
-
     // Your code here
-
   }
 
   static getSmartMove(grid, symbol) {
-
     // Your code here
-
   }
-
 }
 
 module.exports = ComputerPlayer;
