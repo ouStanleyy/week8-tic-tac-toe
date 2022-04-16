4e17400c30e05aabcf82ff2f62a3af776bcf83ec
stanley <ou.stanley@yahoo.com>
2022-04-16 12:15:15 -0400

    computer-player completed.

--- starter/class/computer-player.js
+++ starter/class/computer-player.js
@@ -1,10 +1,12 @@
+const TTT = require("./ttt");
+
 class ComputerPlayer {
   static getValidMoves(grid) {
     const validMoves = [];
 
     for (let row = 0; row < grid.length; row++) {
       for (let col = 0; col < grid[row].length; col++) {
-        if (grid[row][col] === " ") validMoves.push({ row: row, col: col });
+        if (grid[row][col] === " ") validMoves.push({ row, col });
       }
     }
 
@@ -17,11 +19,26 @@ class ComputerPlayer {
   }
 
   static getWinningMoves(grid, symbol) {
-    // Your code here
+    const validMoves = this.getValidMoves(grid);
+
+    for (let move of validMoves) {
+      grid[move.row][move.col] = symbol;
+
+      if (TTT.checkWin(grid) === symbol) {
+        grid[move.row][move.col] = " ";
+        return move;
+      } else grid[move.row][move.col] = " ";
+    }
+
+    return false;
   }
 
   static getSmartMove(grid, symbol) {
-    // Your code here
+    const playerSymbol = symbol === "X" ? "O" : "X";
+    const winningMove = this.getWinningMoves(grid, symbol);
+    const playerWinningMove = this.getWinningMoves(grid, playerSymbol);
+
+    return winningMove ? winningMove : playerWinningMove;
   }
 }
 
